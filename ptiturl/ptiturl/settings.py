b3bc06f0438641b33c4206e000eb0487225b23a8
Itotutona <itotutona@evta.fr>
2022-02-23 00:12:54 +0100

    Update secret key

--- ptiturl/ptiturl/settings.py
+++ ptiturl/ptiturl/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ib!l@f-cn_pzhqbimpuuh%xt8!%4j4d0lj!+86g#^_a))bo*n5'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
