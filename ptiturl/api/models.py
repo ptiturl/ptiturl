648c5da733a004940024b4c75e38968dfec4c19e
Itotutona <itotutona@evta.fr>
2022-02-23 14:49:12 +0100

    Add url model

--- ptiturl/api/models.py
+++ ptiturl/api/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Url(models.Model):
-    pass
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    uri = models.CharField(max_length=10)
+    url = models.URLField(max_length=1000)
+    is_enable = models.BooleanField()
