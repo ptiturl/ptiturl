cbfae7d42e00e2073b87b12e92e52bc60719a21f
Itotutona <itotutona@evta.fr>
2022-02-23 18:42:52 +0100

    Remove drf

--- ptiturl/api/urls.py
+++ ptiturl/api/urls.py
@@ -1,22 +1,6 @@
-from django.urls import path, include
-from rest_framework import routers, serializers, viewsets
+from django.urls import path
+from . import views
 
-# Serializers define the API representation.
-class UrlSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Url
-        fields = ['url', 'username', 'email', 'is_staff']
-
-# ViewSets define the view behavior.
-class UrlViewSet(viewsets.ModelViewSet):
-    queryset = Url.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path("u/<str:slugs>", views.urlRedirect, name="redirect")
+]
