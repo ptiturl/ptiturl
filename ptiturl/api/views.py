e4dd10f1020e4027e01d66006238e4b376c69f53
Itotutona <itotutona@evta.fr>
2022-02-23 18:43:00 +0100

    Add url redirect view

--- ptiturl/api/views.py
+++ ptiturl/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+from .models import Url
+def urlRedirect(request, slugs):
+    try:
+        data = Url.objects.get(uri=slugs)
+    except Exception:
+        return redirect(f"/error/u/{slugs}")
+    else:
+        if data.is_enable:
+            return redirect(data.url)
+        else:
+            return redirect(f"/error/d/{slugs}")
