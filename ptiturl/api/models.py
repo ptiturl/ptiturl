0782c314c11831d665df503f24d1451af806d80b
Itotutona <itotutona@evta.fr>
2022-02-23 15:00:01 +0100

    Add min length on uri field

--- ptiturl/api/models.py
+++ ptiturl/api/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.validators import MinLengthValidator
 # Create your models here.
 
 class Url(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    uri = models.CharField(max_length=10)
+    uri = models.CharField(max_length=10, validators=[MinLengthValidator(10)])
     url = models.URLField(max_length=1000)
     is_enable = models.BooleanField()
